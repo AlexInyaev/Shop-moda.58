84c925b22b43fde86930d10b9e58b828d2f06961
AlexInyaev <46852628+AlexInyaev@users.noreply.github.com>
2019-03-22 18:52:42 +0300

    #

--- js/gallery-full.js
+++ js/gallery-full.js
@@ -2,10 +2,11 @@ var itemFull = document.querySelector('.gallery-full');
 
 
 function testclick (id){
-	console.log (id);
+	console.log (id.style.backgroundImage);
+	
 	
 itemFull.style.display="block";
-itemFull.style.backgroundImage = document.getElementById(id).style.backgroundImage;
+itemFull.style.backgroundImage = id.style.backgroundImage;
 }
 function galClose() {
 	itemFull.style.display="none";
